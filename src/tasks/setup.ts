624d28f282fb00602f33b701c877e3ecea8a3e82
cbrzn <cesarbrazon10@gmail.com>
2021-08-03 22:02:11 +0200

    feat: set up guide almost finished

--- src/tasks/setup.ts
+++ src/tasks/setup.ts
@@ -1,7 +1,7 @@
 import "hardhat-deploy";
 import "@nomiclabs/hardhat-ethers";
 import { task, types } from "hardhat/config";
-import { Contract } from "ethers";
+import { BigNumber } from "ethers";
 
 task("setup", "deploy a SafeExit Module").setAction(
   async (taskArgs, hardhatRuntime) => {
@@ -13,3 +13,26 @@ task("setup", "deploy a SafeExit Module").setAction(
     console.log("SafeExit Module deployed to:", module.address);
   }
 );
+
+task("deployDesignatedToken")
+  .addParam(
+    "user",
+    "User that will receive tokens after deployment",
+    "",
+    types.string
+  )
+  .setAction(async (taskArgs, hardhatRuntime) => {
+    const [caller] = await hardhatRuntime.ethers.getSigners();
+    console.log("Using the account:", caller.address);
+
+    const Token = await hardhatRuntime.ethers.getContractFactory("TestToken");
+    const token = await Token.deploy();
+
+    await token.deployTransaction.wait(3);
+    console.log("Token deployed to:", token.address);
+
+    const receiver = taskArgs.user || caller.address;
+    await token.mint(receiver, BigNumber.from(10).pow(18));
+
+    console.log("Token minted to:", receiver);
+  });
