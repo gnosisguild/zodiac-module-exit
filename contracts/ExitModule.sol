72a406f9893cb1fa2099d223d7a3465a8b91db42
Auryn Macmillan <hi@auryn.xyz>
2021-09-07 14:09:01 -0400

    Update package name

--- contracts/ExitModule.sol
+++ contracts/ExitModule.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@gnosis/zodiac/contracts/core/Module.sol";
+import "@gnosis.pm/zodiac/contracts/core/Module.sol";
 
 import "./CirculatingSupply.sol";
 
