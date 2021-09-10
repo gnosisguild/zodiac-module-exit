e280e730e85e49395bfc6db944f7c76661a7bf8a
Cesar Brazon <cesarbrazon10@gmail.com>
2021-09-10 05:34:50 +0200

    Feat: Deployment tasks (#32)

---
+++ src/deploy/deploy_module.ts
@@ -0,0 +1,26 @@
+import { DeployFunction } from "hardhat-deploy/types";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+
+const FirstAddress = "0x0000000000000000000000000000000000000001";
+
+const deploy: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployments, getNamedAccounts } = hre;
+  const { deployer } = await getNamedAccounts();
+  const { deploy } = deployments;
+  const args = [
+    FirstAddress,
+    FirstAddress,
+    FirstAddress,
+    FirstAddress
+  ];
+
+  await deploy("Exit", {
+    from: deployer,
+    args,
+    log: true,
+    deterministicDeployment: true,
+  });
+};
+
+deploy.tags = ["exit-module"];
+export default deploy;
