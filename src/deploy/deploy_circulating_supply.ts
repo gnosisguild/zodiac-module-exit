ec8624df6ec9e365923dade7513f1d2a8e803226
cbrzn <cesarbrazon10@gmail.com>
2021-09-14 16:22:21 +0200

    feat: deploy singleton contract method implemented - not working yet

--- src/deploy/deploy_circulating_supply.ts
+++ src/deploy/deploy_circulating_supply.ts
@@ -4,17 +4,17 @@ import { HardhatRuntimeEnvironment } from "hardhat/types";
 const FirstAddress = "0x0000000000000000000000000000000000000001";
 
 const deploy: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  const { deployments, getNamedAccounts } = hre;
-  const { deployer } = await getNamedAccounts();
-  const { deploy } = deployments;
-  const args = [FirstAddress, FirstAddress, []];
+  // const { deployments, getNamedAccounts } = hre;
+  // const { deployer } = await getNamedAccounts();
+  // const { deploy } = deployments;
+  // const args = [FirstAddress, FirstAddress, []];
 
-  await deploy("CirculatingSupply", {
-    from: deployer,
-    args,
-    log: true,
-    deterministicDeployment: true,
-  });
+  // await deploy("CirculatingSupply", {
+  //   from: deployer,
+  //   args,
+  //   log: true,
+  //   deterministicDeployment: true,
+  // });
 };
 
 deploy.tags = ["circulating-supply"];
--- src/deploy/deploy_module.ts
+++ src/deploy/deploy_module.ts
@@ -1,26 +1,26 @@
 import { DeployFunction } from "hardhat-deploy/types";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { deploySingletonContract } from "@gnosis.pm/zodiac";
 
+import { bytecode as ExitByteCode } from "../../build/artifacts/contracts/ExitModule.sol/Exit.json";
 const FirstAddress = "0x0000000000000000000000000000000000000001";
 
 const deploy: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  const { deployments, getNamedAccounts } = hre;
-  const { deployer } = await getNamedAccounts();
-  const { deploy } = deployments;
+  const Exit = await hre.ethers.getContractFactory("Exit");
   const args = [
     FirstAddress,
     FirstAddress,
     FirstAddress,
     FirstAddress,
-    FirstAddress
+    FirstAddress,
   ];
+  const encodedParams = Exit.interface.encodeDeploy(args);
 
-  await deploy("Exit", {
-    from: deployer,
-    args,
-    log: true,
-    deterministicDeployment: true,
-  });
+  const deploymentBytecode = hre.ethers.utils.hexConcat([
+    ExitByteCode,
+    encodedParams,
+  ]);
+  await deploySingletonContract(deploymentBytecode, "0xabbbb", hre);
 };
 
 deploy.tags = ["exit-module"];
