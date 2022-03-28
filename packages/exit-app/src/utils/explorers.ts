17bced4e7952433ef84b1fc3c8d382c0d393d982
Carlos Febres <carlosfebres@dorg.tech>
2022-03-28 13:23:05 -0400

    [safe-app] added missing data

--- packages/exit-app/src/utils/explorers.ts
+++ packages/exit-app/src/utils/explorers.ts
@@ -1,59 +1,67 @@
-import {NETWORK} from "./networks";
+import { NETWORK } from './networks'
 
-const REACT_APP_ETHERSCAN_KEY = process.env.REACT_APP_ETHERSCAN_KEY;
+const REACT_APP_ETHERSCAN_KEY = process.env.REACT_APP_ETHERSCAN_KEY
 
 interface ExplorerData {
-  networkExplorerName: string;
-  networkExplorerUrl: string;
-  networkExplorerApiUrl: string;
-  safeTransactionApi: string;
-  safeUrl: string;
-  explorerApiKey?: string;
-  verifyContractUrl: string;
+  networkExplorerName: string
+  networkExplorerUrl: string
+  networkExplorerApiUrl: string
+  safeTransactionApi: string
+  safeUrl: string
+  explorerApiKey?: string
+  verifyContractUrl: string
 }
 
 export const EXPLORERS_CONFIG: Record<NETWORK, ExplorerData> = {
   [NETWORK.MAINNET]: {
-    networkExplorerName: "Etherscan",
-    networkExplorerUrl: "https://etherscan.io",
-    networkExplorerApiUrl: "https://api.etherscan.io/api",
-    safeTransactionApi: "https://safe-transaction.gnosis.io/",
-    safeUrl: "https://gnosis-safe.io/",
-    verifyContractUrl: "https://etherscan.io/verifyContract",
+    networkExplorerName: 'Etherscan',
+    networkExplorerUrl: 'https://etherscan.io',
+    networkExplorerApiUrl: 'https://api.etherscan.io/api',
+    safeTransactionApi: 'https://safe-transaction.gnosis.io/',
+    safeUrl: 'https://gnosis-safe.io/app/eth:',
+    verifyContractUrl: 'https://etherscan.io/verifyContract',
     explorerApiKey: REACT_APP_ETHERSCAN_KEY,
   },
   [NETWORK.RINKEBY]: {
-    networkExplorerName: "Etherscan",
-    networkExplorerUrl: "https://rinkeby.etherscan.io",
-    networkExplorerApiUrl: "https://api-rinkeby.etherscan.io/api",
-    safeTransactionApi: "https://safe-transaction.rinkeby.gnosis.io/",
-    safeUrl: "https://rinkeby.gnosis-safe.io/",
-    verifyContractUrl: "https://rinkeby.etherscan.io/verifyContract",
+    networkExplorerName: 'Etherscan',
+    networkExplorerUrl: 'https://rinkeby.etherscan.io',
+    networkExplorerApiUrl: 'https://api-rinkeby.etherscan.io/api',
+    safeTransactionApi: 'https://safe-transaction.rinkeby.gnosis.io/',
+    safeUrl: 'https://gnosis-safe.io/app/rin:',
+    verifyContractUrl: 'https://rinkeby.etherscan.io/verifyContract',
     explorerApiKey: REACT_APP_ETHERSCAN_KEY,
   },
   [NETWORK.XDAI]: {
-    networkExplorerName: "Blockscout",
-    networkExplorerUrl: "https://blockscout.com/poa/xdai",
-    networkExplorerApiUrl: "https://blockscout.com/xdai/mainnet/api",
-    safeUrl: "https://xdai.gnosis-safe.io/",
-    safeTransactionApi: "https://safe-transaction.xdai.gnosis.io/",
-    verifyContractUrl:
-      "https://docs.blockscout.com/for-users/smart-contract-interaction/verifying-a-smart-contract",
+    networkExplorerName: 'Blockscout',
+    networkExplorerUrl: 'https://blockscout.com/poa/xdai',
+    networkExplorerApiUrl: 'https://blockscout.com/xdai/mainnet/api',
+    safeUrl: 'https://gnosis-safe.io/app/gno:',
+    safeTransactionApi: 'https://safe-transaction.xdai.gnosis.io/',
+    verifyContractUrl: 'https://docs.blockscout.com/for-users/smart-contract-interaction/verifying-a-smart-contract',
   },
   [NETWORK.POLYGON]: {
-    networkExplorerName: "Polygonscan",
-    networkExplorerUrl: "https://polygonscan.com",
-    networkExplorerApiUrl: "https://api.polygonscan.com/api",
-    safeUrl: "https://polygon.gnosis-safe.io/",
-    safeTransactionApi: "https://safe-transaction.polygon.gnosis.io/",
-    verifyContractUrl: "https://polygonscan.com/verifyContract",
+    networkExplorerName: 'Polygonscan',
+    networkExplorerUrl: 'https://polygonscan.com',
+    networkExplorerApiUrl: 'https://api.polygonscan.com/api',
+    safeUrl: 'https://gnosis-safe.io/app/matic:',
+    safeTransactionApi: 'https://safe-transaction.polygon.gnosis.io/',
+    verifyContractUrl: 'https://polygonscan.com/verifyContract',
     explorerApiKey: REACT_APP_ETHERSCAN_KEY,
   },
-};
+  [NETWORK.BSC]: {
+    networkExplorerName: 'Bscscan',
+    networkExplorerUrl: 'https://bscscan.com/',
+    networkExplorerApiUrl: 'https://bscscan.com/api',
+    safeUrl: 'https://gnosis-safe.io/app/bsc:',
+    safeTransactionApi: 'https://safe-transaction.bsc.gnosis.io/',
+    verifyContractUrl: 'https://bscscan.com/verifyContract',
+    explorerApiKey: REACT_APP_ETHERSCAN_KEY,
+  },
+}
 
 export const getNetworkExplorerInfo = (chainId: number) => {
-  const networkBaseConfig = EXPLORERS_CONFIG[chainId as NETWORK];
-  if (!networkBaseConfig) return;
+  const networkBaseConfig = EXPLORERS_CONFIG[chainId as NETWORK]
+  if (!networkBaseConfig) return
   return {
     name: networkBaseConfig.networkExplorerName,
     url: networkBaseConfig.networkExplorerUrl,
@@ -62,15 +70,15 @@ export const getNetworkExplorerInfo = (chainId: number) => {
     safeTransactionApi: networkBaseConfig.safeTransactionApi,
     safeUrl: networkBaseConfig.safeUrl,
     verifyUrl: networkBaseConfig.verifyContractUrl,
-  };
-};
+  }
+}
 
 export const getExplorerInfo = (chainId: number, hash: string) => {
-  const explorerData = getNetworkExplorerInfo(chainId);
-  if (!explorerData) return;
-  const type = hash.length > 42 ? "tx" : "address";
+  const explorerData = getNetworkExplorerInfo(chainId)
+  if (!explorerData) return
+  const type = hash.length > 42 ? 'tx' : 'address'
   return () => ({
     url: `${explorerData.url}/${type}/${hash}`,
     alt: explorerData.name,
-  });
-};
+  })
+}
