8fc85677dc26e96d1e0ccc0e583b125e150e6087
Carlos Febres <carlosfebres@dorg.tech>
2022-03-28 13:27:52 -0400

    [safe-app] remove unused functions

--- packages/exit-app/src/utils/explorers.ts
+++ packages/exit-app/src/utils/explorers.ts
@@ -72,13 +72,3 @@ export const getNetworkExplorerInfo = (chainId: number) => {
     verifyUrl: networkBaseConfig.verifyContractUrl,
   }
 }
-
-export const getExplorerInfo = (chainId: number, hash: string) => {
-  const explorerData = getNetworkExplorerInfo(chainId)
-  if (!explorerData) return
-  const type = hash.length > 42 ? 'tx' : 'address'
-  return () => ({
-    url: `${explorerData.url}/${type}/${hash}`,
-    alt: explorerData.name,
-  })
-}
